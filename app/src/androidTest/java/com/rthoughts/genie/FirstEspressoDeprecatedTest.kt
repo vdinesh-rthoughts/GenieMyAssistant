f195fbe6259d1879907242cf2a48ff2cb07fe3e3
DINESH KUMAR V <dineshkumar.codoid@gmail.com>
2020-10-03 00:57:40 +0530

    Espresso Instrumentation test with new and deprecated classes

--- app/src/androidTest/java/com/rthoughts/genie/FirstEspressoDeprecatedTest.kt
+++ app/src/androidTest/java/com/rthoughts/genie/FirstEspressoDeprecatedTest.kt
@@ -5,25 +5,25 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
+import androidx.test.runner.AndroidJUnit4
 import androidx.test.uiautomator.UiDevice
 import org.hamcrest.core.AllOf.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4ClassRunner::class)
-class FirstEspressoTest {
+@RunWith(AndroidJUnit4::class)
+class FirstEspressoDeprecatedTest {
 
     private val mUiDevice: UiDevice =
         UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
     @Rule
     @JvmField
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    val activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Rule
     @JvmField
